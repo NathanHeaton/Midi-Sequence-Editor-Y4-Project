5f0ec80110d2a882ed01535b606216f0746de3d1
nathanheaton <nathanmheaton@gmail.com>
2025-10-11 14:10:04 +0100

    got project working on home pc after 2 hours

--- src/MainComponent.cpp
+++ src/MainComponent.cpp
@@ -16,7 +16,7 @@ void MainComponent::paint (juce::Graphics& g)
 
     g.fillAll (MyColours::background);
 
-    g.setFont (juce::FontOptions (30.0f));
+    g.setFont (juce::FontOptions (45.0f));
     g.setColour (juce::Colours::white);
     g.drawText ("Test Project", getLocalBounds(), juce::Justification::centred, true);
 }
