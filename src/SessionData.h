277ab3ce55b072fbed196bd85490d21a0b26e90c
nathanheaton <nathanmheaton@gmail.com>
2025-11-30 17:30:57 +0000

    setup SessionData singleton to hold and manage Session data

---
+++ src/SessionData.h
@@ -0,0 +1,63 @@
+//
+// Created by nathan on 30/11/2025.
+//
+
+#ifndef MYPROJECT_SESSIONDATA_H
+#define MYPROJECT_SESSIONDATA_H
+
+
+#include <juce_core/juce_core.h>
+#include <juce_events/juce_events.h>
+#include <juce_data_structures/juce_data_structures.h>
+
+#include "UI/Piano_Roll/Panels/PianoRoll/PianoKeys.h"
+
+class SessionData {
+    public:
+    static SessionData& instance() {
+        static SessionData instance;
+        return instance;
+    }
+
+    int getBPM() const {return BPM;}
+    int getNumerator() const {return numerator;}
+    int getDenominator() const {return denominator;}
+    float getBarWidth() const {return barWidth;}
+
+    void setBPM(int newBPM) {
+        BPM = newBPM;
+        listeners.call(&Listener::bpmChanged, newBPM);
+    }
+
+    void setNumerator(int newNumerator) {
+        numerator = newNumerator;
+        listeners.call(&Listener::numeratorChanged, newNumerator);
+    }
+
+    void setDenominator(int newDenominator) {
+        denominator = newDenominator;
+        listeners.call(&Listener::denominatorChanged, newDenominator);
+    }
+
+    class Listener {
+        public:
+        virtual ~Listener() =default;
+        virtual void bpmChanged(int newBPM)=0;
+        virtual void denominatorChanged(int newDenominator)=0;
+        virtual void numeratorChanged(int newNumerator)=0;
+    };
+
+    juce::ListenerList<Listener> listeners;
+
+    private:
+    JUCE_DECLARE_NON_COPYABLE(SessionData);
+
+    int BPM =120;
+    int numerator =4;
+    float barWidth = 200.0f;
+    int denominator =4;
+
+
+
+};
+#endif //MYPROJECT_SESSIONDATA_H
--- src/UI/Piano_Roll/Panels/PianoRoll/BarLabels.h
+++ src/UI/Piano_Roll/Panels/PianoRoll/BarLabels.h
@@ -1,8 +1,78 @@
 //
-// Created by nathan on 30/11/2025.
+// Created by nathan on 21/11/2025.
 //
 
-#ifndef MYPROJECT_BARLABELS_H
-#define MYPROJECT_BARLABELS_H
+#include "../../../../Misc/MyColours.h"
+#include <juce_gui_extra/juce_gui_extra.h>
+#include "../../../../SessionData.h"
 
-#endif //MYPROJECT_BARLABELS_H
+class BarLabels : public juce::Component,
+public SessionData::Listener
+{
+public:
+
+    float barWidth = SessionData::instance().getBarWidth();
+    int beatsPerBar = SessionData::instance().getDenominator();
+    float tempDivision = barWidth / static_cast<float>(beatsPerBar);
+
+    // --- Constructor ---
+    BarLabels()
+    {
+        setSize(3000, 30);
+        SessionData::instance().listeners.add(this);
+    }
+
+    void denominatorChanged(int newDenominator) override {
+        beatsPerBar = newDenominator;
+        tempDivision = barWidth / static_cast<float>(beatsPerBar);
+        resized();
+    }
+
+
+
+
+    // --- Component Painting ---
+    void paint(juce::Graphics& g) override {
+
+        g.setColour(MyColours::outline);
+        g.drawRoundedRectangle(getLocalBounds().toFloat(), 0.0f, 2.0f);
+    }
+
+
+    // --- Component Layout and Resizing ---
+    void resized() override
+    {
+        const int numBarsNeeded = static_cast<int>(std::ceil(getWidth() / barWidth));
+
+        while (labelComponents.size() > numBarsNeeded)
+        {
+            removeChildComponent(labelComponents.back().get());
+            labelComponents.pop_back();
+        }
+
+        for (int i = labelComponents.size(); i < numBarsNeeded; i++)
+        {
+            auto newLabel = std::make_unique<juce::Label>(
+                "timline_" + std::to_string(i),
+                std::to_string(i)
+            );
+            addAndMakeVisible(*newLabel);
+            labelComponents.push_back(std::move(newLabel));
+        }
+        juce::FlexBox row;
+        row.flexDirection = juce::FlexBox::Direction::row;
+        row.alignItems = juce::FlexBox::AlignItems::stretch;
+
+        for (const auto& lblPtr : labelComponents)
+        {
+            row.items.add(juce::FlexItem(*lblPtr)
+                .withWidth(barWidth)
+                .withHeight(25.0f));
+        }
+        row.performLayout(getLocalBounds().withHeight(25)); // Only layout the top 25px for labels
+    }
+
+private:
+    std::vector<std::unique_ptr<juce::Label>> labelComponents;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(BarLabels)
+};
